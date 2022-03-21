0367da6b6def43c5c1989f573af6d53906b314c4
BelT26 <helen.taylor@hotmail.it>
2022-03-21 11:41:51 +0000

    add product and category models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    """A model to created categories that the products will be divided into"""
+    name = models.CharField(max_length=200, blank=False, null=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    """A model to represent each of the products sold"""
+    name = models.CharField(max_length=200, blank=False, null=False)
+    category = models.ForeignKey(Category, null=True, blank=True,
+                                 on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True,
+                                 blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
--- products/views.py
+++ products/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def all_products(request):
+    """returns a view of all available products"""
+    return render(request, 'products/all_products.html')
