bf4416d6a266eb8180679f35599f55edb771448c
BelT26 <helen.taylor@hotmail.it>
2022-03-21 15:55:51 +0000

    add product details to all_products template

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
+    path('category/<category>', views.category, name='category'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from .models import Product, Category
 
 
 # Create your views here.
 def all_products(request):
     """returns a view of all available products"""
-    return render(request, 'products/all_products.html')
+    products = Product.objects.all()
+    context = {
+        'products': products,
+    }
+    return render(request, 'products/all_products.html', context)
+
+
+def category(request, category):
+    """returns a page with information and products relating to the selected category"""
+    context = {
+        'category': category,
+    }
+    return render(request, 'products/category.html', context)
