b061d631a703dbb510504a8d97b6ba9cd3354b22
BelT26 <helen.taylor@hotmail.it>
2022-03-21 12:05:15 +0000

    add fields to Category model

--- products/models.py
+++ products/models.py
@@ -10,6 +10,10 @@ class Category(models.Model):
         verbose_name_plural = 'Categories'
 
     name = models.CharField(max_length=200, blank=False, null=False)
+    location = models.CharField(max_length=200, blank=True, null=True)
+    description = models.TextField(default='')
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
