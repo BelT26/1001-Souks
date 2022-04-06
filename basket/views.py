d41fcf97db6519e4522f564b3920fc17781f6116
BelT26 <helen.taylor@hotmail.it>
2022-04-06 15:08:10 +0000

    add form and view to allow user to add items to their basket

--- basket/views.py
+++ basket/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
-def basket(request):
+def view_basket(request):
     """returns a view of the contents of the basket"""
     return render(request, 'basket/basket.html')
+
+
+def add_to_basket(request, item_id):
+    """allows user to add items to their shopping basket"""
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    basket = request.session.get('basket', {})
+
+    if item_id in list(basket.keys()):
+        basket[item_id] += quantity
+    else:
+        basket[item_id] = quantity
+
+    request.session['basket'] = basket
+    print(request.session['basket'])
+    return redirect(redirect_url)
