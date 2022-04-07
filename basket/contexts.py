f9b7f762cbe16f1c9a896b4ffda56fdd584542e7
BelT26 <helen.taylor@hotmail.it>
2022-04-07 16:09:19 +0000

    update contexts file

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,3 +1,9 @@
+from django.shortcuts import get_object_or_404
+from products.models import Product
+from django.conf import settings
+# the below code is based on the Boutique Ado walkthrough project
+
+
 def basket_contents(request):
     """"makes basket accessible across all apps"""
     basket_items = []
@@ -5,11 +11,20 @@ def basket_contents(request):
     product_count = 0
     basket = request.session.get('basket', {})
 
+    for item_id, quantity in basket.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        basket_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
     context = {
         'basket_items': basket_items,
         'total': total,
         'product_count': product_count,
-        'basket': basket
     }
 
     return context
