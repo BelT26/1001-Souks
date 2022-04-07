de9cc624649fb057952c53210d00b9b1b8537691
BelT26 <helen.taylor@hotmail.it>
2022-04-07 14:08:52 +0000

    create cards for all products page

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,13 +1,15 @@
 def basket_contents(request):
-
+    """"makes basket accessible across all apps"""
     basket_items = []
     total = 0
     product_count = 0
+    basket = request.session.get('basket', {})
 
     context = {
         'basket_items': basket_items,
         'total': total,
         'product_count': product_count,
+        'basket': basket
     }
 
     return context
