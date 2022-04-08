2a86059db00c46c783846e4b4caf8645f48e9d7e
BelT26 <helen.taylor@hotmail.it>
2022-04-08 08:53:22 +0000

    add colour and size selection to product template. style custom button.

--- map/models.py
+++ map/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from products.models import Category
-
 
 # Create your models here.
 class City(models.Model):
@@ -12,8 +10,6 @@ class City(models.Model):
         verbose_name_plural = 'Cities'
 
     name = models.CharField(max_length=200, blank=False, null=False)
-    category = models.ForeignKey(Category, null=True, blank=True,
-                                 on_delete=models.SET_NULL)
     description = models.TextField(default='')
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
--- products/models.py
+++ products/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from map.models import City
 
 
 # Create your models here.
 class Category(models.Model):
-    """A model to represent the categories that the products will be divided into"""
+    """A model to represent the categories that the products will be
+    divided into"""
 
     class Meta:
         """returns the correct plural spelling in the admin panel"""
@@ -14,6 +16,8 @@ class Category(models.Model):
     description = models.TextField(default='')
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    city = models.ForeignKey(City, null=True, blank=True,
+                             on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.name
@@ -27,10 +31,19 @@ class Product(models.Model):
     sku = models.CharField(max_length=200, null=True, blank=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True,
+    rating = models.DecimalField(max_digits=2, decimal_places=1, null=True,
                                  blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    size_selection = models.BooleanField(default=False, null=True, blank=True)
+    colour_selection = models.BooleanField(default=False, null=True,
+                                           blank=True)
+    maker = models.CharField(max_length=200, blank=True, null=True)
+    city = models.ForeignKey(City, null=True, blank=True,
+                             on_delete=models.SET_NULL)
+    product_type = models.CharField(max_length=200, blank=True, null=True)
+    composition = models.CharField(max_length=200, null=True, blank=True)
+
 
     def __str__(self):
         return self.name
