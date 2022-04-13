bcac1b544130bad6b83567beccf339c5b6c7f963
BelT26 <helen.taylor@hotmail.it>
2022-04-13 15:25:43 +0000

    register City model

--- map/admin.py
+++ map/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import City
+
 
 # Register your models here.
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    """determines how category details are displayed in the admin panel"""
+    list_display = ('name',)
+    ordering_by = ('name',)
