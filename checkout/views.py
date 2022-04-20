f0fb60ad2c0a756879c4f2e7757b44ccd884b610
BelT26 <helen.taylor@hotmail.it>
2022-04-20 16:20:36 +0000

    add initial stripe settings to checkout app

--- checkout/views.py
+++ checkout/views.py
@@ -1,9 +1,12 @@
 # adapted from Boutique Ado project
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from basket.contexts import basket_contents
 
+import stripe
 
 def checkout(request):
     """
@@ -16,8 +19,14 @@ def checkout(request):
         messages.error(request, 'Your basket is currently empty')
         return redirect(reverse('home'))
 
+    current_basket = basket_contents(request)
+    total = current_basket['total']
+    stripe_total = round(total * 100)
+
     form = OrderForm()
     
     return render(request, 'checkout/checkout.html', {
         'form': form,
+        'stripe_public_key': 'pk_test_51KVCjXLzPYZbKFCeKtJpmj7yQ4hByR7OUxl0NVO8EKsr4qBviIwtAE53x3k9ikM2TWIJJK0wLG0atu1LJi5sPj1i00isbVub7I',
+        'client_secret': 'test',
     })
