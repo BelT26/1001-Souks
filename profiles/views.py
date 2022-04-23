826bd639b5c3be6cc8e864afa8f34e266648a943
BelT26 <helen.taylor@hotmail.it>
2022-04-23 13:53:01 +0000

    format allauth templates and update view for profile app

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import UserProfile
 
 
 def profile(request):
-    """ Displays the user's profile. """    
-    context = {}
+    """ Displays the user's profile. """
+    profile = get_object_or_404(UserProfile, user=request.user)    
 
-    return render(request, 'profiles/profile.html', context)
+    return render(request, 'profiles/profile.html', {
+        'profile': profile
+    })
