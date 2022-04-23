fb7dcdd6a136ba20c9c250cac546cfe1ff6e9ce3
BelT26 <helen.taylor@hotmail.it>
2022-04-23 09:06:12 +0000

    amend country field on checkout form

--- checkout/forms.py
+++ checkout/forms.py
@@ -15,8 +15,7 @@ class OrderForm(forms.ModelForm):
         model = Order
         fields = ('full_name', 'email', 'phone_number',
                   'street_address1', 'street_address2',
-                  'town_or_city', 'postcode', 'country',
-                  'county',)
+                  'town_or_city', 'postcode', 'county',)
 
     def __init__(self, *args, **kwargs):
         """
@@ -38,10 +37,11 @@ class OrderForm(forms.ModelForm):
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
             self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
