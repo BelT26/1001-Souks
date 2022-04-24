94e67aea315aaeb1045f0550273d9925058e264d
BelT26 <helen.taylor@hotmail.it>
2022-04-24 11:43:22 +0000

    update checkout views and add order history table to profile template

--- checkout/views.py
+++ checkout/views.py
@@ -8,6 +8,8 @@ from .forms import OrderForm
 from .models import Order, OrderLineItem
 from products.models import Product
 from basket.contexts import basket_contents
+from profiles.models import UserProfile
+from profiles.forms import UserProfileForm
 
 import stripe
 import json
@@ -107,16 +109,32 @@ def checkout(request):
             amount=stripe_total,
             currency=settings.STRIPE_CURRENCY,
         )
-        print(intent)
 
-        form = OrderForm()
-
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(initial={
+                    'full_name': profile.user.get_full_name(),
+                    'email': profile.default_email,
+                    'phone_number': profile.default_phone_number,
+                    'country': profile.default_country,
+                    'postcode': profile.default_postcode,
+                    'town_or_city': profile.default_town_or_city,
+                    'street_address1': profile.default_street_address1,
+                    'street_address2': profile.default_street_address2,
+                    'county': profile.default_county,
+                })
+            except UserProfile.DoesNotExist:
+                order_form = OrderForm()
+        else:
+            order_form = OrderForm()
+      
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
             Did you forget to set it in your environment?')
 
     return render(request, 'checkout/checkout.html', {
-        'form': form,
+        'form': order_form,
         'stripe_public_key': stripe_public_key,
         'client_secret': intent.client_secret,
     })
@@ -128,6 +146,30 @@ def checkout_success(request, order_number):
     """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
+
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Attach the user's profile to the order
+        order.user_profile = profile
+        order.save()
+
+        # Save the user's info
+        if save_info:
+            profile_data = {
+                'default_email': order.email,
+                'default_phone_number': order.phone_number,
+                'default_country': order.country,
+                'default_postcode': order.postcode,
+                'default_town_or_city': order.town_or_city,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_county': order.county,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+
+
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
         email will be sent to {order.email}.')
--- profiles/views.py
+++ profiles/views.py
@@ -4,6 +4,8 @@ from django.contrib import messages
 from .models import UserProfile
 from .forms import UserProfileForm
 
+from checkout.models import Order
+
 
 def profile(request):
     """ Displays the user's profile. """
@@ -22,4 +24,21 @@ def profile(request):
         'orders': orders,
         'form': form,
         'on_profile_page': True
+    })
+
+
+def order_history(request, order_number):
+    """
+    displays details of past orders
+    """
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'The original confirmation was sent on the order date.'
+    ))
+
+    return render(request, 'checkout/checkout_success.html', {
+        'order': order,
+        'from_profile': True,
     })
