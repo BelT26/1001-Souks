baf14afcbb6d32a292d412c55df4cf96b16ee767
BelT26 <helen.taylor@hotmail.it>
2022-04-24 10:03:30 +0000

    reformat toast success and correct variable typo in profile view

--- checkout/forms.py
+++ checkout/forms.py
@@ -15,7 +15,8 @@ class OrderForm(forms.ModelForm):
         model = Order
         fields = ('full_name', 'email', 'phone_number',
                   'street_address1', 'street_address2',
-                  'town_or_city', 'postcode', 'county',)
+                  'town_or_city', 'postcode', 'county',
+                  'country')
 
     def __init__(self, *args, **kwargs):
         """
--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
     """ Displays the user's profile. """
-    profile = get_object_or_404(UserProfile, user=request.user)    
+    user_profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=user_profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+
+    form = UserProfileForm(instance=user_profile)
+    orders = user_profile.orders.all()
 
     return render(request, 'profiles/profile.html', {
-        'profile': profile
+        'orders': orders,
+        'form': form,
+        'on_profile_page': True
     })
