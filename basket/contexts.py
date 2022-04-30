3e2becd1d49aec5f83c37e9b785d2f3accb77bc3
BelT26 <helen.taylor@hotmail.it>
2022-04-30 10:03:29 +0000

    add multibuy calculation for items without colours

--- basket/contexts.py
+++ basket/contexts.py
@@ -14,19 +14,34 @@ def basket_contents(request):
     for item_id, item_data in basket.items():
         if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
-            total += item_data * product.price
-            subtotal = item_data * product.price
             product_count += item_data
+            if product.multibuy_offer:
+                if item_data >= product.multibuy_num_items:
+                    num_offers = item_data // product.multibuy_num_items
+                    extras = item_data % product.multibuy_num_items
+                    multibuy_amount = num_offers * product.multibuy_total
+                    extras_amount = extras * product.price
+                    subtotal = multibuy_amount + extras_amount
+                    total += multibuy_amount + extras_amount
+                else:
+                    total += item_data * product.price
+                    subtotal = item_data * product.price
+            else:
+                total += item_data * product.price
+                subtotal = item_data * product.price  
             basket_items.append({
                 'item_id': item_id,
                 'quantity': item_data,
                 'product': product,
                 'subtotal': subtotal
-            }) 
+            })
 
         else:
             product = get_object_or_404(Product, pk=item_id)
             for colour, quantity in item_data['items_by_colour'].items():
+                item_names = []
+                item_names.append(product.name)
+                print(item_names)
                 total += quantity * product.price
                 subtotal = quantity * product.price
                 product_count += quantity
