7f8c01f6d9aa8bf903f7c96e68832ba6d2f07e6d
BelT26 <helen.taylor@hotmail.it>
2022-05-02 19:04:33 +0000

    correct typo in settings.py

--- souks/settings.py
+++ souks/settings.py
@@ -114,7 +114,7 @@ EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')}
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
