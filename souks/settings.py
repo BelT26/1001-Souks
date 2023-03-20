a8c3492cd94f9baac6a8af6bade43031b6853e67
BelT26 <helen.taylor@hotmail.it>
2023-03-20 16:35:04 +0000

    "deploy to Render"

--- souks/settings.py
+++ souks/settings.py
@@ -31,6 +31,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 DEBUG = False
 
 ALLOWED_HOSTS = ['souks-1001.herokuapp.com', 'localhost']
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 # Application definition
 
